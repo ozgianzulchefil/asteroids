b9587fa6a4fb8b47217a364cdfa3efe61bf7ac88
whitenight09 <ozzy@mail.com>
2025-08-02 22:13:10 +0300

    updated code

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     color = (0, 0, 0)
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill(color)
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()
