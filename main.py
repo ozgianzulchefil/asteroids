082d93547e58dfe01f1a51893f9726f2bcf01deb
whitenight09 <ozzy@mail.com>
2025-08-03 13:29:18 +0300

    corrected player class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -12,10 +12,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player =  Player(x,y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,7 +21,9 @@ def main():
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = (clock.tick(60)) / 1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
